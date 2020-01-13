dd7419182372ebcf62cdb287099d7ff13b4d34fa
Larry Li <lingyv.li@gmail.com>
2020-01-13 14:54:06 +1100

    add maven

--- .travis/before-install-linux-dart.sh
+++ .travis/before-install-linux-dart.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 sudo apt-get update -qq
 
-sudo apt-get install mvn
+sudo apt-get install maven
 
 mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V
