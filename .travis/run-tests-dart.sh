2eb1c0339eaad14e7ca8dc9b9a430e88e0c9c4e8
Larry Li <lingyv.li@gmail.com>
2020-01-19 13:45:39 +1100

    try debug pipeline

--- .travis/run-tests-dart.sh
+++ .travis/run-tests-dart.sh
@@ -2,4 +2,6 @@
 
 set -euo pipefail
 
+echo OSEIGJGVOKSNEOGJNSO
+ls /home/travis/build/lingyv-li/antlr4/runtime-testsuite/target/classes/Dart/
 mvn -q -Dparallel=methods -DthreadCount=4 -Dtest=dart.* test
