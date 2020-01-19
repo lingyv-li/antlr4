9cd221496710067d33e732eb0532f43a997f3204
Larry Li <lingyv.li@gmail.com>
2020-01-19 17:06:41 +1100

    debug test

--- .travis/run-tests-dart.sh
+++ .travis/run-tests-dart.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 set -euo pipefail
+ls /home/travis/build/lingyv-li/antlr4/runtime-testsuite/target/classes
+ls /home/travis/build/lingyv-li/antlr4/runtime-testsuite/target/classes/Dart
+ls /home/travis/build/lingyv-li/antlr4/runtime-testsuite/target/classes/Dart/lib
+ls /home/travis/build/lingyv-li/antlr4/runtime-testsuite/target/classes/Dart/lib/src
+ls /home/travis/build/lingyv-li/antlr4/runtime-testsuite/target/classes/Dart/lib/src/atn
 mvn -q -Dparallel=classes -DthreadCount=4 -Dtest=dart.* test
