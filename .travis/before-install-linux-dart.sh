7d203260826ceb1900ddbb4026f9be856c0e17ff
Larry Li <lingyv.li@gmail.com>
2020-01-19 16:15:20 +1100

    debug test

--- .travis/before-install-linux-dart.sh
+++ .travis/before-install-linux-dart.sh
@@ -7,6 +7,3 @@ sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | ap
 sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
 sudo apt-get -q update
 sudo apt-get -y install dart
-
-mvn -DskipTests clean
-mvn -DskipTests install --batch-mode
